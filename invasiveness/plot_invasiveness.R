15b9878be0b9ef201d6435d595acdb17c4ffeeee
Tommi Mäklin <tommi@maklin.fi>
2022-06-15 11:15:26 +0200

    Update invasiveness plots.

--- invasiveness/plot_invasiveness.R
+++ invasiveness/plot_invasiveness.R
@@ -1,29 +1,27 @@
 
+source("plot_subplot.R")
+
 odds.ratios <- read.table("NORM_OR_E_coli_carriage_disease.csv", sep = ',', header = TRUE)
-
 in.plot <- odds.ratios$Labe != ""
-
 plotting.data <- odds.ratios[in.plot, ]
-
+plotting.data <- plotting.data[grepl("NORM", plotting.data$Label), ]
+plotting.data$Label <- gsub("_SC[0-9]*[[:space:]]*\\(NORM\\)", "", plotting.data$Label)
 labels.order <- order(as.numeric(gsub("[*]", "", gsub("ST", "", plotting.data$Label))), decreasing = TRUE)
-
+plotting.data.all <- plotting.data[labels.order, ]
+odds.ratios <- read.table("ST131_clades_OR_E_coli_carriage_disease_collapsed.csv", sep = ',', header = TRUE)
+in.plot <- odds.ratios$Labe != ""
+plotting.data <- odds.ratios[in.plot, ]
+labels.order <- order(as.numeric(gsub("[*]", "", gsub("ST", "", plotting.data$Label))), decreasing = TRUE)
 plotting.data <- plotting.data[labels.order, ]
+plotting.data.st131 <- plotting.data[c(12:1, 14, 13, 15), ]
 
-pdf(file = "invasiveness_vs_carriage.pdf", width = 5, height = 5)
-par(mar = c(4.5, 4, 0.5, 0.5))
-plot(0, 0, pch = '', xlim = c(-1.0, 7.5), ylim = c(1, nrow(plotting.data)), xaxt = 'n', yaxt = 'n', xlab = 'Odds ratio', ylab = '', bty = 'n')
-axis(side = 1, at = seq(0.0, 7.0, by = 1.0), labels = c("0.0", "1.0", "2.0", "3.0", "4.0", "5.0", "6.0", "7.0"))
-axis(side = 2, at = 1:nrow(plotting.data), labels = plotting.data$Label, las = 2)
-abline(v = 1.0, lty = "dotted", col = "gray40")
-for (i in 1:nrow(plotting.data)) {
-    x.coords <- c(plotting.data$lower[i], plotting.data$upper[i])
-    y.coords <- c(i, i)
-    linecol <- "gray80"
-    if (plotting.data$p.adj.BH[i] < 0.05) {
-        linecol <- ifelse(plotting.data$lower[i] > 1.0, "#d73027", "#92c5de")
-    }
-    lines(x = x.coords, y = y.coords, col = linecol, lwd = 2)
-}
-lines(x = plotting.data$OR, y = 1:nrow(plotting.data), type = 'p', pch = 19)
-legend("bottomright", legend = c("Odds ratio", "95% confidence interval", "Invasive", "Intermediate", "Commensal"), col = c("black", "black", "#d73027", "gray80", "#92c5de"), bty = 'n', lty = c(NA, 1, 1, 1, 1), lwd = c(NA, 2, 2, 2, 2), pch = c(19, NA, NA, NA, NA))
+pdf(file = "invasiveness_vs_carriage.pdf", width = 8, height = 5)
+layout(matrix(c(1, 2, 3, 3), byrow = TRUE, nrow = 2, ncol = 2), height = c(6, 1))
+par(mar = c(4.5, 4, 0.5, 2.5))
+PlotInvasiveness(plotting.data.all)
+par(mar = c(4.5, 6, 0.5, 0.5))
+PlotInvasiveness(plotting.data.st131)
+par(mar = c(0, 0, 0, 0))
+plot(0, bty ='n', xaxt='n', yaxt='n', xlab='', ylab='', pch = '')
+legend("top", legend = c("Odds ratio", "95% confidence interval", "", "Invasive", "Intermediate", "Commensal"), col = c("black", "black", "black", "#d73027", "gray80", "#92c5de"), bty = 'n', lty = c(NA, 1, NA, 1, 1, 1), lwd = c(NA, 2, NA, 2, 2, 2), pch = c(19, NA, NA, NA, NA, NA), ncol = 2)
 dev.off()
---
+++ invasiveness/plot_subplot.R
@@ -0,0 +1,17 @@
+
+PlotInvasiveness <- function(plotting.data) {
+    plot(0, 0, pch = '', xlim = c(-1.0, 7.5), ylim = c(1, nrow(plotting.data)), xaxt = 'n', yaxt = 'n', xlab = 'Odds ratio', ylab = '', bty = 'n')
+    axis(side = 1, at = seq(0.0, 7.0, by = 1.0), labels = c("0.0", "1.0", "2.0", "3.0", "4.0", "5.0", "6.0", "7.0"))
+    axis(side = 2, at = 1:nrow(plotting.data), labels = plotting.data$Label, las = 2)
+    abline(v = 1.0, lty = "dotted", col = "gray40")
+    for (i in 1:nrow(plotting.data)) {
+        x.coords <- c(plotting.data$lower[i], plotting.data$upper[i])
+        y.coords <- c(i, i)
+        linecol <- "gray80"
+        if (plotting.data$p.adj.BH[i] < 0.05) {
+            linecol <- ifelse(plotting.data$lower[i] > 1.0, "#d73027", "#92c5de")
+        }
+        lines(x = x.coords, y = y.coords, col = linecol, lwd = 2)
+    }
+    lines(x = plotting.data$OR, y = 1:nrow(plotting.data), type = 'p', pch = 19)
+}
