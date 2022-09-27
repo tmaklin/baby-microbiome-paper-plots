83718334343197805a780d97d5260657c690a34e
Tommi Mäklin <tommi@maklin.fi>
2022-09-27 14:29:31 +0300

    Hide the Infancy-included plots and include the transition counts in the matrices.

--- transition_matrix/plotting.R
+++ transition_matrix/plotting.R
@@ -3,6 +3,19 @@ library("plot.matrix")
 library("psych")
 library("dichromat")
 
+shadowtext <- function(x, y=NULL, labels, col='white', bg='black',
+	theta= seq(pi/4, 2*pi, length.out=8), r=0.1, ... ) {
+	xy <- xy.coords(x,y)
+	xo <- r*strwidth('A')
+	yo <- r*strheight('A')
+	for (i in theta) {
+		text( xy$x + cos(i)*xo, xy$y + sin(i)*yo,
+		  labels, col=bg, ... )
+
+	}
+	text(xy$x, xy$y, labels, col=col, ... )
+}
+
 PlotTransitionMatrix <- function(transition.matrix, title.main, title.adj, title.line, y.lab.pos, n.max, negadj, ColorFunc) {##correlations, obs.counts, otu.names, title.main, title.adj, ColorFunc) {
     n.states <- nrow(transition.matrix)
     state.names <- colnames(transition.matrix)
@@ -31,20 +44,23 @@ PlotTransitionMatrix <- function(transition.matrix, title.main, title.adj, title
                 color.id <- transition.matrix[i, j]
 
                 ##size <- log(obs.counts[i, j] + 1, base = 6) + 1
-                size <- 2
-                lines(x = coord.x, y = coord.y, type = 'p', pch = 19,
+                size <- 3.5
+                lines(x = coord.x, y = coord.y, type = 'p', pch = 15,
                       cex = size, col = ColorFunc(n.max)[color.id])
+                shadowtext(x = coord.x, y = coord.y, labels = transition.matrix[i, j], bg = "black", cex = 1.4)
+                ##text(x = coord.x, y = coord.y, labels = transition.matrix[i, j], cex = 2.5, col = "black", font = 2)
+                ##text(x = coord.x, y = coord.y, labels = transition.matrix[i, j], cex = 2.5, col = "gray90")
             }
         }
     }
     title(main = title.main, line = title.line, adj = title.adj, outer = TRUE)
 }
 
-IntensityLegend <- function(ColorFunc, n.max, title.vadj = -37.5) {
+IntensityLegend <- function(ColorFunc, n.max, title.vadj = -26) {
     par(xpd = TRUE)
     plot(c(0,2),c(0,10),type = 'n', axes = F,xlab = '', ylab = '')
     title(main = "Transitions", font.main = 1, cex.main = 1.31,
           line = title.vadj, adj = 0)
-    legend("left", legend = seq(1, n.max, l = n.max), col = ColorFunc(n.max), bty = 'n', pch = 19, cex = 1.7)
+    legend("left", legend = seq(1, n.max, l = n.max), col = ColorFunc(n.max), bty = 'n', pch = 15, cex = 1.7)
     par(xpd = FALSE)
 }
